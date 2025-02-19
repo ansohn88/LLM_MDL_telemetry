126c880c5eca4b12ea3467befe96a3acb79fb9d1
Andrew Sohn <90402167+ansohn88@users.noreply.github.com>
2025-02-19 08:57:02 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,8 +4,22 @@ import polars as pl
 from typing import Union
 
 
+def is_instance_initialized(instance):
+    return bool(instance.__dict__)
+
+
 def load_txt_as_json(filepath: str) -> list:
     return json.load(open(filepath, "r"))
+
+
+def to_json(file_list: list) -> list:
+    file_list_json = [load_json_file(f) for f in file_list]
+    file_list_json = [element for sublist in file_list_json for element in sublist]
+    return file_list_json
+    
+
+def number_of_samples(json_data: list) -> int:
+    return len(json_data)
 
 
 def find_files_with_extension(
@@ -48,3 +62,25 @@ def filter_by_quantile(
         pl.col(col) > df[col].quantile(lower_bound, interp)
     )
     return df
+
+
+def search_listdict_by_keys(
+        list_of_dicts: list[dict],
+        values_to_match: list[str],
+        keys_to_search: list[str] = ["MRN", "SpecimenCollectionDate"]
+    ) -> str:
+    # values_to_match = ["2235965", "2024-10-04T10:37:00"]
+    filtered_data = []
+    for dictionary in list_of_dicts:
+        match = True
+        for key, value in zip(keys_to_search, values_to_match):
+            if dictionary[key] != value:
+                match = False
+                break
+        if match:
+            filtered_data.append(dictionary)
+    
+    if len(filtered_data) == 0:
+        return "Flow cytometry report is not available."
+    else:
+        return filtered_data[0]["Intepretation"]
